d2b5f0d21a47b631e6af5139cdde2de39a110b4d
Ishika Thakur <ishikathakur0708@gmail.com>
2024-05-02 03:23:15 +0530

    first commit

---
+++ src/main/java/com/ishika/chatroom/Message.java
@@ -0,0 +1,34 @@
+package com.ishika.chatroom;
+
+public class Message {
+    private String name;
+    private String content;
+
+
+    public Message(String name, String content)
+    {
+        this.name=name;
+        this.content=content;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+
+    public String getContent()
+    {
+        return content;
+    }
+
+    public void setContent(String content)
+    {
+        this.content=content;
+    }
+
+}
